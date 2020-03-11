450643b54f514a902c0cd574b99e548f3b9d5165
Richard Peterson <richardpeterson@Roys-Air.localdomain>
2020-03-10 17:06:19 -0700

    implemented all of the functionality in js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Let's get this party started!");
+$(function(){
+
+    let $giphyAddButton = $("button#addGiphy");
+
+    $giphyAddButton.on("click",async function() {
+        let $giphyInput =  $("input").val();
+        let $response = await axios.get(`http://api.giphy.com/v1/gifs/search?api_key=4JXpQ427AGRNzlryZzUYDgjuI5w01j8T&q=${$giphyInput}&limit:1&offset:8`);
+        console.log($response.data.data[8].url);
+        let $image = $(`<img src="https://i.giphy.com/media/${$response.data.data[8].id}/giphy.webp">`);
+        let $container = $("section");
+
+        $container.append($image);
+
+    })
+
+    let $clearButton = $("button#clearGiphys");
+
+    $clearButton.on("click", function() {
+        let $container = $("section");
+        $container.empty()
+    })
+
+});
+
